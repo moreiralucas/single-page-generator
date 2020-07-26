971cf5795e91dd1ac7967f7438b661477317c397
Lucas Moreira Pires <lucas-mopi@hotmail.com>
2020-07-26 17:40:27 -0300

    Update input example and implement generate code

--- generator.py
+++ generator.py
@@ -1,13 +1,41 @@
-import os
+import os, json
 from jinja2 import Template
 
-template = Template('Hello {{ name }}!')
-template.render(name='John Doe')
+def check_item(items):
+  for data in items:
+    for key, value in data.items():
+      # Optional params
+      if key == "btn_size" or key == "btn_color" or key == "path_image":
+        continue
 
-itens = []
+      if value == "":
+        raise "This param is required"
 
-template = env.get_templates(os.path.join('templates','index.html'))
+def check_data(data):
+  for key, value in data.items():
+    # Optional params
+    if key == "footer_phrase" or key == "footer" or key == "path_logo_title":
+      continue
 
-print(template.render(itens=itens))
+    if value == "":
+      raise "This param is required"
 
+    if key == "items":
+      check_item(value)
+
+def get_json():
+  with open("input.json", "r") as f:
+    input_file = f.read()
+    return json.loads(input_file)
+
+def get_template():
+  with open("templates/index.html", "r") as f:
+    template_file = f.read()
+    template = Template(template_file)
+    return template
+
+template = get_template()
+dados = get_json()
+check_data(dados)
+print(template.render(dados))
 
